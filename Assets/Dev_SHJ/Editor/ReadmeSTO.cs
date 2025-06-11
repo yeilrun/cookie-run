2be1323b9d6c9aaa505d16297a6ed33509b127e7
huijung <huijung2097@naver.com>
2025-06-11 11:05:15 +0900

    shj 250611 readme finish

--- Assets/Dev_SHJ/Editor/ReadmeSTO.cs
+++ Assets/Dev_SHJ/Editor/ReadmeSTO.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ReadmeSTO", menuName = "Scriptable Objects/ReadmeSTO")]
 public class ReadmeSTO : ScriptableObject
 {
-    
+    public string gameStartCamera = string.Empty;
+    public string gameMain1Camera = string.Empty;
+    public string gameMain2Camera = string.Empty;
+    public string gamePlayCamera = string.Empty;
 }
