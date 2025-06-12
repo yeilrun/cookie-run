00b270dddfe3ae0f03e9870793f5caa076ecd2e8
huijung <huijung2097@naver.com>
2025-06-12 14:11:19 +0900

    shj 250612 join

--- Assets/Dev_SHJ/Scripts/GameLoading.cs
+++ Assets/Dev_SHJ/Scripts/GameLoading.cs
@@ -11,10 +11,15 @@ namespace SHJ
         public delegate void LoginCallback();
         public static event LoginCallback loginCallback;
         
-        [SerializeField] private TMP_InputField username;
-        [SerializeField] private TMP_InputField password;
-
+        [SerializeField] private TMP_InputField loginUsername;
+        [SerializeField] private TMP_InputField loginPassword;
+        
+        [SerializeField] private GameObject joinGo;
+        [SerializeField] private TMP_InputField joinUsername;
+        [SerializeField] private TMP_InputField joinPassword;
+        
         private string loginURL = "http://127.0.0.1:8000/api-token-auth/";
+        private string joinURL = "http://127.0.0.1:8000/api-auth/create/";
         
         private struct Token
         {
@@ -29,9 +34,9 @@ namespace SHJ
         
         public void Login()
         {
-            if (username.text != string.Empty && password.text != string.Empty)
+            if (loginUsername.text != string.Empty && loginPassword.text != string.Empty)
             {
-                StartCoroutine(LoginRequest(username.text, password.text));
+                StartCoroutine(LoginRequest(loginUsername.text, loginPassword.text));
             }
         }
 
@@ -47,6 +52,41 @@ namespace SHJ
             {
                 sToken = JsonConvert.DeserializeObject<Token>(res.downloadHandler.text);
                 loginCallback?.Invoke();
+            }
+        }
+
+        public void OnJoin()
+        {
+            joinUsername.text = string.Empty;
+            joinPassword.text = string.Empty;
+            joinGo.SetActive(true);
+        }
+
+        public void OnJoinClose()
+        {
+            joinGo.SetActive(false);
+        }
+        
+        public void Join()
+        {
+            if (joinUsername.text != string.Empty && joinUsername.text != string.Empty)
+            {
+                StartCoroutine(JoinRequest(joinUsername.text, joinPassword.text));
+            }
+        }
+
+        private IEnumerator JoinRequest(string username, string password)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("username", username);
+            form.AddField("password", password);
+            UnityWebRequest res = UnityWebRequest.Post(joinURL, form);
+            yield return res.SendWebRequest();
+
+            if (res.result == UnityWebRequest.Result.Success)
+            {
+                joinGo.SetActive(false);
+                Debug.Log("success");
             }
         }
     }
