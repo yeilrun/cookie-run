fa71be65473c73682a986907e056c672e7492522
cho <148734999+cho4920@users.noreply.github.com>
2025-06-17 11:43:55 +0900

    CSH 20250617 add time

--- Assets/Dev_CSH/Scripts/time.cs
+++ Assets/Dev_CSH/Scripts/time.cs
@@ -1,56 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;   // Silder class 사용하기 위해 추가합니다.
 
-
-namespace CSH
+public class GameManager : MonoBehaviour
 {
-    public class time : MonoBehaviour
+
+    // 시간을 표시하는 text UI를 유니티에서 가져온다.
+    public Text gameTimeUI;
+
+    // 전체 제한 시간을 설정해준다. 여기서는 300초.
+     float setTime = 300;
+
+    // 분단위와 초단위를 담당할 변수를 만들어준다.
+    [SerializeField] int min;
+    [SerializeField] float sec;
+
+    void Update()
     {
+        // 남은 시간을 감소시켜준다.
+        setTime -= Time.deltaTime;
 
-        [SerializeField] public TMP_Text timerText;
-        [SerializeField] public float countdownTime;
-        [SerializeField] private bool timerRunning;
-
-        public void Start()
+        // 전체 시간이 60초 보다 클 때
+        if (setTime >= 60f)
         {
-            startTimer();
+            // 60으로 나눠서 생기는 몫을 분단위로 변경
+            min = (int)setTime / 60;
+            // 60으로 나눠서 생기는 나머지를 초단위로 설정
+            sec = setTime % 60;
+            // UI를 표현해준다
+            gameTimeUI.text = min + ":" + (int)sec;
         }
 
-        public void Update()
+        // 전체시간이 60초 미만일 때
+        if (setTime < 60f)
         {
-            if (timerRunning)
-            {
-                if (countdownTime > 0)
-                {
-                    countdownTime-=Time.deltaTime;
-                    updateTimeDisplay();
-                }
-
-                else
-                {
-                    countdownTime = 0;
-                    timerRunning=false;
-                    TimeFinished();
-                }
-            }
+            // 분 단위는 필요없어지므로 초단위만 남도록 설정
+            gameTimeUI.text = "0:"+(int)sec;
         }
 
-        public void startTimer()
+        // 남은 시간이 0보다 작아질 때
+        if (setTime <= 0)
         {
-            timerRunning = true;
-        }
-
-        public void updateTimeDisplay()
-        {
-            int minutes = Mathf.FloorToInt(countdownTime/60);
-            int seconds = Mathf.FloorToInt(countdownTime%60);
-            timerText.text=string.Format("{0:D2}:{1:D2}", minutes, seconds);
-        }
-
-        public void TimeFinished()
-        {
-            Debug.Log("0:00");
+            // UI 텍스트를 0초로 고정시킴.
+            gameTimeUI.text = "0:00";
         }
     }
-
 }
